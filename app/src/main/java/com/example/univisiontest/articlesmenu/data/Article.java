3df4b69d865d80190d7ed2880375dd4f5bdffb43
Andres Velasquez <VLASQUEZ53069@HOTMAIL.COM>
2019-01-25 18:15:16 -0500

    DAO

--- app/src/main/java/com/example/univisiontest/articlesmenu/data/Article.java
+++ app/src/main/java/com/example/univisiontest/articlesmenu/data/Article.java
@@ -1,4 +1,14 @@
 package com.example.univisiontest.articlesmenu.data;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "articles")
 public class Article {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo("id")
+    private v
 }
